99a07d2590527acfc434880de8cdeed55af6efda
tarynmcmillan <taryn.mcmillan@gmail.com>
2023-09-21 13:31:10 -0400

    Add more isInteracting checks

--- Assets/PlayerTest.cs
+++ Assets/PlayerTest.cs
@@ -14,18 +14,43 @@ public class PlayerTest : MonoBehaviour
     public LayerMask groundLayer;
     public LayerMask chestLayer;
 
+    private PlayerInput playerInput;
+    private InputAction touchPositionAction;
+    private InputAction touchPressAction;
+    private float movement;
+    
+
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        touchPressAction = playerInput.actions["TouchJump"];
+        touchPositionAction = playerInput.actions["TouchMove"];
+        rb = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
+    }
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         isInteracting = false;
     }
 
+    private void OnEnable()
+    {
+        touchPressAction.performed += TouchTap;
+        touchPositionAction.performed -= TouchMove;
+    }
+
+    private void OnDisable()
+    {
+        touchPressAction.performed -= TouchTap;
+        touchPositionAction.performed -= TouchMove;
+
+    }
     private void Update()
     {
         // Ground check using a small circle overlap
-        
+
         isInteracting = Physics2D.OverlapCircle(groundCheck.position, 0.2f, chestLayer);
-        print(isInteracting);
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
 
         // Handle keyboard input for movement
@@ -39,25 +64,47 @@ public class PlayerTest : MonoBehaviour
         //}
     }
 
-   
     private void FixedUpdate()
     {
-        // Handle movement
         rb.velocity = new Vector2(moveInput.x * moveSpeed, rb.velocity.y);
     }
+    public void TouchTap(InputAction.CallbackContext context)
+    {
+        if (isInteracting)
+        {
+            OnJump(context);
+            OnInteract(context);
+        }
 
+    }
+    public void TouchMove(InputAction.CallbackContext context)
+    {
+        if (!isInteracting)
+        {
+            Vector2 position = Camera.main.ScreenToWorldPoint(context.ReadValue<Vector2>());
+            if (context.ReadValue<Vector2>().x > (0.5 * Screen.width))
+            {
+                MoveRight();
+            }
+            else
+            {
+                MoveLeft();
+            }
+        }
+
+    }
     public void OnMove(InputAction.CallbackContext context)
     {
-        // Get the input vector from the Input System
-        moveInput = context.ReadValue<Vector2>();
+        if (!isInteracting)
+        {
+            moveInput = context.ReadValue<Vector2>();
+        }
     }
 
     public void OnJump(InputAction.CallbackContext context)
     {
-        Debug.Log($"{context.started}{isGrounded}{isInteracting}");
         if (context.started && isGrounded && !isInteracting)
         {
-            
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
     }
@@ -72,7 +119,6 @@ public class PlayerTest : MonoBehaviour
         moveInput.x = 1f;
     }
    
-
     public void OnInteract(InputAction.CallbackContext context)
     {
         
